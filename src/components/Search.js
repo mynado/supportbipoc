e89a166713e4a6e97cb62a4c40300781b0250ed9
MyNa Do <myna.do@bimobject.com>
2020-12-16 16:13:57 +0100

    boilerplate for searchbar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Search = () => {
+	const [searchQuery, setSearchQuery] = useState(null)
+
+	const handleChange = (e) => {
+		setSearchQuery(e.target.value)
+		console.log(searchQuery)
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+
+		console.log('search', searchQuery)
+	}
 	return (
 		<>
-			<form>
-				<div className="form-group d-flex">
-					<input type="text" className="form-control" id="search-query" aria-describedby="search" placeholder="Search..." />
-					<button type="submit">🔍</button>
+			<form onSubmit={handleSubmit}>
+				<div className="search-bar form-group d-flex">
+					<input onChange={handleChange} type="text" className="search-bar-input form-control" id="search-query" aria-describedby="search" placeholder="Search..." />
+					<button className="search-bar-button" type="submit">🔍</button>
 				</div>
-				
 			</form>
 		</>
 	)
