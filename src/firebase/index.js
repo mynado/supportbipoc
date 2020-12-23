1d2ef066155a9dd61266397e68db046494269a29
MyNa Do <myna.do@mahoyo.com>
2020-12-23 11:08:45 +0100

    reinstalled firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 // get configuration from environment
 const firebaseConfig = {
