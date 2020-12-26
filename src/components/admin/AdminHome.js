ed610c5114d0d22446d5bc05cb0bfb56ba82daec
MyNa Do <myna.do@mahoyo.com>
2020-12-26 15:18:24 +0100

    added links in admin panel

--- src/components/admin/AdminHome.js
+++ src/components/admin/AdminHome.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AdminHome = () => {
 	return (
 		<div>
-			<p>Admin Panel</p>
+			<h1>Adminpanel</h1>
+			<p>Här kan du lägga till, ta bort och redigera företag.</p>
+			<ul>
+				<li><Link to="/companies/add">Lägg till företag</Link></li>
+				<li><Link to="/companies">Visa alla företag</Link></li>
+			</ul>
+
 		</div>
 	)
 }
