905823929ddadc4cff361170896819373bb14528
MyNa Do <myna.do@mahoyo.com>
2020-12-27 13:22:42 +0100

    some styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -17,61 +17,63 @@ const Navigation = () => {
 	}
 	return (
 		<>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+		<div className={open ? 'menu-open' : 'menu-collapse'} id="navbarNav">
+				<span onClick={handleClickMenu}>X</span>
+				<ul className="navbar-nav">
+					{
+						currentUser ? (
+							<div className="navbar-admin">
+								<li className="nav-item">
+									<NavLink to="/admin/home">
+										Adminpanel
+									</NavLink>
+								</li>
+								<li className="nav-item">
+									<NavLink to="/companies">
+										Visa alla företag
+									</NavLink>
+								</li>
+								<li className="nav-item">
+									<NavLink to="/admin/add-company">
+										Lägg till företag
+									</NavLink>
+								</li>
+								<li className="nav-item">
+									<NavLink to="/admin" onClick={handleLogout}>Logout</NavLink>
+								</li>
+							</div>
+						) : (
+							<div className="navbar-user">
+								<li className="nav-item active">
+									<NavLink to="/">Home <span className="sr-only">(current)</span></NavLink>
+								</li>
+								<li className="nav-item active">
+									<NavLink to="/">Kategorier <span className="sr-only">(current)</span></NavLink>
+								</li>
+								<li className="nav-item active">
+									<NavLink to="/">Om oss <span className="sr-only">(current)</span></NavLink>
+								</li>
+								<li className="nav-item active">
+									<NavLink to="/">Kontakt <span className="sr-only">(current)</span></NavLink>
+								</li>
+							</div>
+
+						)
+					}
+				</ul>
+			</div>
+			<nav className="navbar navbar-expand-lg">
 				<div className="container">
-					<Link to="/" className="navbar-brand">
+					<NavLink to="/" className="navbar-brand">
 						Fuck Your Fusion
-					</Link>
+					</NavLink>
 
 					<button className="navbar-toggler" onClick={handleClickMenu}>
-						<span className="navbar-toggler-icon"></span>
+						<span>Menu</span>
 					</button>
 				</div>
 			</nav>
-			<div className={open ? 'menu-open' : 'menu-collapse'} id="navbarNav">
-					<ul className="navbar-nav">
-						{
-							currentUser ? (
-								<div className="navbar-admin">
-									<li className="nav-item">
-										<NavLink to="/admin/home">
-											Adminpanel
-										</NavLink>
-									</li>
-									<li className="nav-item">
-										<NavLink to="/companies">
-											Visa alla företag
-										</NavLink>
-									</li>
-									<li className="nav-item">
-										<NavLink to="/admin/add-company">
-											Lägg till företag
-										</NavLink>
-									</li>
-									<li className="nav-item">
-										<NavLink to="/admin" onClick={handleLogout}>Logout</NavLink>
-									</li>
-								</div>
-							) : (
-								<div className="navbar-user">
-									<li className="nav-item active">
-										<NavLink to="/">Home <span className="sr-only">(current)</span></NavLink>
-									</li>
-									<li className="nav-item active">
-										<NavLink to="/">Kategorier <span className="sr-only">(current)</span></NavLink>
-									</li>
-									<li className="nav-item active">
-										<NavLink to="/">Om oss <span className="sr-only">(current)</span></NavLink>
-									</li>
-									<li className="nav-item active">
-										<NavLink to="/">Kontakt <span className="sr-only">(current)</span></NavLink>
-									</li>
-								</div>
-
-							)
-						}
-					</ul>
-				</div>
+			
 
 		</>
 	)
