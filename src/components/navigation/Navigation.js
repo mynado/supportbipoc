32cde6817544faeccdc5c398a3b33134c71c3545
MyNa Do <myna.do@mahoyo.com>
2020-12-30 15:05:50 +0100

    close menu when click outside nav and close menu when click on nav link

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,27 +1,47 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
 import logo from '../../assets/icons/logo/logo-outline-web.png'
 import './Navigation.scss'
 
 const Navigation = () => {
+	const node = useRef();
 	const { currentUser, logout } = useAuth()
 	const [open, setOpen] = useState(false)
 
 	const handleClickMenu = () => {
 		setOpen(!open)
-		console.log(open)
+	}
+
+	const handleClickOutside = (e) => {
+		if (node.current.contains(e.target)) {
+			return
+		}
+		setOpen(false)
 	}
 
 	const handleLogout = () => {
 		logout()
+		setOpen(!open)
 	}
+
+	useEffect(() => {
+		if (open) {
+		  document.addEventListener("mousedown", handleClickOutside);
+		} else {
+		  document.removeEventListener("mousedown", handleClickOutside);
+		}
+	
+		return () => {
+		  document.removeEventListener("mousedown", handleClickOutside);
+		};
+	  }, [open]);
 	return (
 		<>
 			<nav className="navbar navbar-expand-lg">
 				<div className="container">
 					<NavLink to="/" className="navbar-brand">
-					<img src={logo} className="navbar-logo" alt="logo" /> 
+						<img src={logo} className="navbar-logo" alt="logo" /> 
 					</NavLink>
 
 					<button className={`navbar-toggler ${open ? 'open' : ''}` } onClick={handleClickMenu} id="nav-icon">
@@ -32,24 +52,24 @@ const Navigation = () => {
 					</button>
 				</div>
 			</nav>
-			<div className={`navbar-mobile ${open ? 'menu-open' : 'menu-collapse'}`}>
+			<div className={`navbar-mobile ${open ? 'menu-open' : 'menu-collapse'}`} ref={node}>
 				<div className="navbar-mobile-container">
 					<ul className="navbar-mobile-list">
 						{
 							currentUser ? (
 								<div className="navbar-admin">
 									<li className="nav-item">
-										<NavLink to="/admin/home">
+										<NavLink to="/admin/home" onClick={handleClickMenu}>
 											Adminpanel
 										</NavLink>
 									</li>
 									<li className="nav-item">
-										<NavLink to="/companies">
+										<NavLink to="/companies" onClick={handleClickMenu}>
 											Visa alla företag
 										</NavLink>
 									</li>
 									<li className="nav-item">
-										<NavLink to="/admin/add-company">
+										<NavLink to="/admin/add-company" onClick={handleClickMenu}>
 											Lägg till företag
 										</NavLink>
 									</li>
@@ -60,24 +80,23 @@ const Navigation = () => {
 							) : (
 								<div className="navbar-user">
 									<li className="nav-item active">
-										<NavLink to="/">Home <span className="sr-only">(current)</span></NavLink>
+										<NavLink to="/" onClick={handleClickMenu}>Home <span className="sr-only">(current)</span></NavLink>
 									</li>
 									<li className="nav-item active">
-										<NavLink to="/">Kategorier <span className="sr-only">(current)</span></NavLink>
+										<NavLink to="/categories" onClick={handleClickMenu}>Kategorier <span className="sr-only">(current)</span></NavLink>
 									</li>
 									<li className="nav-item active">
-										<NavLink to="/">Om oss <span className="sr-only">(current)</span></NavLink>
+										<NavLink to="/about" onClick={handleClickMenu}>Om oss <span className="sr-only">(current)</span></NavLink>
 									</li>
 									<li className="nav-item active">
-										<NavLink to="/">Kontakt <span className="sr-only">(current)</span></NavLink>
+										<NavLink to="/contact" onClick={handleClickMenu}>Kontakt <span className="sr-only">(current)</span></NavLink>
 									</li>
 								</div>
-
 							)
 						}
 					</ul>
 				</div>
-			</div>			
+			</div>		
 		</>
 	)
 }
