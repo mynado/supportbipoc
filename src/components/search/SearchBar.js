d643682dbb0251b7e8ed7f685e105124c77392d8
MyNa Do <myna.do@bimobject.com>
2021-05-01 12:36:50 +0200

    remove filter

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,45 +1,20 @@
-import React, { useContext, useState, useEffect, useRef } from 'react'
+import React, { useContext } from 'react'
 import { useLocation } from 'react-router-dom'
 import { SearchContext } from '../../contexts/SearchContext'
-import useClickOutside from '../../hooks/useClickOutside'
 import { FiSearch } from 'react-icons/fi'
-// import { IoFilterSharp } from 'react-icons/io5'
 import './SearchBar.scss'
-// import SearchFilter from './SearchFilter'
 
 const SearchBar = ({ open }) => {
-	const node = useRef()
 	const appContext = useContext(SearchContext)
 	const { handleSubmit, handleChange } = appContext
 	const search = useLocation().search;
 	const query = new URLSearchParams(search).get('q');
-	const [openFilter, setOpenFilter] = useState(false)
-
-	// const handleOpenFilter = () => {
-	// 	setOpenFilter(!openFilter)
-	// }
-
-	useClickOutside(node, () => {
-		if (openFilter) {
-			setOpenFilter(!openFilter)
-		}
-	});
-	
-
-	useEffect(() => {
-		if (openFilter) {
-			document.body.style.overflow = 'hidden'
-		} else {
-			document.body.style.overflow = 'unset'
-		}
-	}, [openFilter])
 
 	return (
 		<>
 			<div className={`search-bar-container ${open ? 'search-bar-menu-open' : 'search-bar-menu-close'}`}>
 				<form onSubmit={handleSubmit} className="search-bar-form">
 					<div className="search-bar form-group d-flex">
-						{/* <div className="filter-button" onClick={handleOpenFilter}><IoFilterSharp /></div> */}
 						<input 
 							onChange={handleChange}
 							type="text"
@@ -50,9 +25,6 @@ const SearchBar = ({ open }) => {
 							/>
 						<button className="search-bar-button" type="submit"><FiSearch /></button>
 					</div>
-					{/* <div className="search-filter">
-						{ openFilter &&(<div ref={node}><SearchFilter /></div>) }
-					</div> */}
 				</form>
 			</div>
 		</>
