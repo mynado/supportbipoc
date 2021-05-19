5880c2f9d905dddeb09ecfbd495d49faf3ce1cd7
MyNa Do <myna.do@bimobject.com>
2021-05-19 10:42:46 +0200

    remove unused icon

--- src/components/companies/CompanyPage.js
+++ src/components/companies/CompanyPage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useParams, useNavigate, Link } from 'react-router-dom'
 import { FiEdit } from 'react-icons/fi'
-import { FaRegAddressCard } from 'react-icons/fa'
 import { HiOutlinePhone, HiLink, HiOutlineMail } from 'react-icons/hi'
 import { useAuth } from '../../contexts/AuthContext'
 import useCompany from '../../hooks/useCompany'
